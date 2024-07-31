f0f445342052d61aeac01281e459ff51dd746da1
gitsetu <pedroroyo@hotmail.com>
2024-07-31 21:48:20 +0100

    navigation menu shows on small screens

--- controllers/about-controller.js
+++ controllers/about-controller.js
@@ -4,6 +4,10 @@ export const aboutController = {
       title: "About Station",
     };
     console.log("about rendering");
+    const timestamp = Date.toLocaleString();
+    const dateint = Intl.DateTimeFormat;
+    const customdatetime = dayjs().format('YYYY-MM-DD HH:mm:ss.SSS');
+    console.log(customdatetime);
     response.render("about-view", viewData);
   },
 };
